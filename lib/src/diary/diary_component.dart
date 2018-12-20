803b815870db79c741e2cea4ce7d9e7584f90eb7
qingfeng1016 <1204145685@qq.com>
2018-12-20 14:42:46 +0800

    first steps

---
+++ lib/src/diary/diary_component.dart
@@ -0,0 +1,25 @@
+import 'package:angular/angular.dart';
+import 'package:angular_components/angular_components.dart';
+import 'dart:html';
+
+@Component(
+  selector: 'diary',
+  styleUrls: ['diary_component.css'],
+  templateUrl: 'diary_component.html',
+  directives: [
+    MaterialCheckboxComponent,
+    MaterialFabComponent,
+    MaterialIconComponent,
+    materialInputDirectives,
+    NgFor,
+    NgIf,
+  ],
+)
+
+class DiaryComponent {
+  
+  //构造函数
+  DiaryComponent(){
+    
+  }
+}
